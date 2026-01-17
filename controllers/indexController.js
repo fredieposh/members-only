fd57b0025b0d3e222822a1a400887c82d9773c40
fredieposh <maor1234@gmail.com>
2026-01-17 17:01:28 +0200

    Add logic of connected user to index page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,10 @@
 exports.getIndexPage = async function(req, res, next ) {
+    const isUserLogin = req.isAuthenticated();
+    const user = isUserLogin ? req.user : null;
     res.render('indexPage', {
         title: "Home Page",
+        isUserLogin,
+        user,
         // messages,
     })
 };
